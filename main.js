3c53d8390299fe1b4ab8207a41b8bb1dd6b7eb87
sebo76 <105569059+sebo76@users.noreply.github.com>
2022-05-17 22:02:23 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+function alertbtn() {
+    create_random_string(12);
+    function create_random_string(string_length) {
+    
+        var random_string = '';
+        var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    
+        
+        for(var i, i  = 0; i < string_length; i++) {
+            random_string += characters.charAt(Math.floor(Math.random() * characters.length ))
+        }
+        alert("Du hast ein Easteregg gefunden! Du erhältst 20% Rabatt 😍                         Hier dein Code: "+random_string);
+
+    }
+
+
+}
